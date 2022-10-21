ad68848b41fbd39e4835bd9f74f64031bd77a7a4
Maverick-Run <95555000+Maverick-Run@users.noreply.github.com>
2022-10-22 11:12:37 +1300

    Update MyTLSFileServer.java

--- MyTLSFileServer.java
+++ MyTLSFileServer.java
@@ -8,131 +8,135 @@ import javax.naming.ldap.*;
 import javax.net.*;
 
 public class MyTLSFileServer {
+
+    static OutputStream clientOutput = null;
+    static BufferedInputStream fileInput = null;
+    static SSLSocket s = null;
+    static SSLServerSocket ss = null;
+    static InputStream clientInput = null;
+    static String filename = null;
+
     public static void main(String[] args) {
-            /*
-             * Get a password input from the console use it in getting our SSF then delete it straight after 
-             */
-            Console cons;
-            char[] passwd;
-            ServerSocketFactory ssf = null;
-        if ((cons = System.console()) != null && (passwd = cons.readPassword("[%s]", "Password: ")) != null){
+        /*
+         * Get a password input from the console use it in getting our SSF then delete
+         * it straight after
+         */
+        Console cons;
+        char[] passwd;
+        ServerSocketFactory ssf = null;
+        if ((cons = System.console()) != null && (passwd = cons.readPassword("[%s]", "Password: ")) != null) {
+            //use password we got
             ssf = getSSF(passwd);
+            //empty our password array now
             java.util.Arrays.fill(passwd, ' ');
         }
         /*
-        * use the getSSF (above) method to get a
-        * SSLServerSocketFactory and create our
-        * SSLServerSocket, bound to specified port
-        */
-        SSLServerSocket ss = null;
+         * use the getSSF (above) method to get a
+         * SSLServerSocketFactory and create our
+         * SSLServerSocket, bound to specified port
+         */
         try {
-            ss =
-            (SSLServerSocket) ssf.createServerSocket(40202);
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+            ss = (SSLServerSocket) ssf.createServerSocket(40202);
+        } catch (Exception e){
+            System.out.println("Error creating socket!");
+            return;
         }
-        String EnabledProtocols[] =
-        {"TLSv1.2", "TLSv1.3"};
+        //Setup the SSL Protocols we want to use (For our case, 1.2 & 1.3)
+        String EnabledProtocols[] = { "TLSv1.2", "TLSv1.3" };
         ss.setEnabledProtocols(EnabledProtocols);
         System.out.println("Listening on port:" + ss.getLocalPort());
-        SSLSocket s = null; 
+        //Accept the incoming connection
         try {
-            s = (SSLSocket)ss.accept();
+            s = (SSLSocket) ss.accept();
         } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+            System.out.println("Handshake failed!");
+            //e1.printStackTrace();
+            return;
         }
         System.out.println("New connection!");
-        InputStream clientInput = null;
+        //Get our streams from the connected client
         try {
             clientInput = s.getInputStream();
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            System.out.println("Failed to get input stream?");
-            e1.printStackTrace();
-        }
-        InputStreamReader is = new InputStreamReader(clientInput);
-        BufferedReader reader = new BufferedReader(is);
-        OutputStream clientOutput = null;
-
-        try {
             clientOutput = s.getOutputStream();
         } catch (IOException e1) {
-            System.out.println("Failed to get output stream?");
-            e1.printStackTrace();
+            System.out.println("Failed to get i/o streams from the socket!");
+            //e1.printStackTrace();
+            return;
         }
-        PrintWriter writer = new PrintWriter(clientOutput);
-        String filename = null;
+        //Setup the readers of the socket
+        InputStreamReader is = new InputStreamReader(clientInput);
+        BufferedReader reader = new BufferedReader(is);
+
         try {
+            //Read in the filename (Should be the first thing sent to us)
             filename = reader.readLine();
-            System.out.println("Sending File: " + filename);
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-        BufferedInputStream fileInput = null;
-        try {
-            fileInput = new BufferedInputStream(new FileInputStream(filename));
+            File readFile = new File(filename);
+            fileInput = new BufferedInputStream(new FileInputStream(readFile));
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+                System.out.print("File not found! Exiting");
+                return;
+        } catch (IOException e1) {
+            System.out.println("Error Reading the filename? Has the connection closed?");
+            return;
         }
+
         int count = 0;
         byte[] buffer = new byte[1024];
         try {
-            while((count = fileInput.read(buffer))>0){
+            while ((count = fileInput.read(buffer)) > 0) {
                 clientOutput.write(buffer, 0, count);
             }
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.out.println("Error in read/write loop!");
+            //e.printStackTrace();
+            return;
         }
     }
 
     private static ServerSocketFactory getSSF(char[] passPhrase) {
-        try{
+        try {
             /*
-            * Get an SSL Context that speaks some version
-            * of TLS, a KeyManager that can hold certs in * X.509 format, and a JavaKeyStore (JKS)
-            * instance
-            */
+             * Get an SSL Context that speaks some version
+             * of TLS, a KeyManager that can hold certs in * X.509 format, and a
+             * JavaKeyStore (JKS)
+             * instance
+             */
             SSLContext ctx = SSLContext.getInstance("TLS");
             KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
             KeyStore ks = KeyStore.getInstance("JKS");
             /*
-            * load the keystore file. The passhrase is
-            * an optional parameter to allow for integrity
-            * checking of the keystore. Could be null
-            */
+             * load the keystore file. The passhrase is
+             * an optional parameter to allow for integrity
+             * checking of the keystore. Could be null
+             */
             ks.load(new FileInputStream("server.jks"), passPhrase);
             /*
-            * init the KeyManagerFactory with a source
-            * of key material. The passphrase is necessary
-            * to unlock the private key contained.
-            */
+             * init the KeyManagerFactory with a source
+             * of key material. The passphrase is necessary
+             * to unlock the private key contained.
+             */
             kmf.init(ks, passPhrase);
             /*
-            * initialise the SSL context with the keys.
-            */
+             * initialise the SSL context with the keys.
+             */
             ctx.init(kmf.getKeyManagers(), null, null);
             /*
-            * get the factory we will use to create
-            * our SSLServerSocket
-            */
+             * get the factory we will use to create
+             * our SSLServerSocket
+             */
             SSLServerSocketFactory ssf = ctx.getServerSocketFactory();
             return ssf;
-            
-        }catch(KeyStoreException kse){
+
+        } catch (KeyStoreException kse) {
             System.out.println("KeyStoreException in getSSF()");
             kse.printStackTrace();
-        }catch(NoSuchAlgorithmException nsae){
+        } catch (NoSuchAlgorithmException nsae) {
             System.out.println("No Such Algorithm Exception in getSSF()");
             nsae.printStackTrace();
-        }catch(IOException ioe){
+        } catch (IOException ioe) {
             System.out.println("IOException in getSSF()");
             ioe.printStackTrace();
-        }catch(CertificateException ce){
+        } catch (CertificateException ce) {
             System.out.println("CertificateException in getSSF()");
             ce.printStackTrace();
         } catch (UnrecoverableKeyException uke) {
